8ff3d3e6f1306b74d4053093cd7e614403aa8a54
Hilmi Raditya <raditya113@icloud.com>
2024-07-18 18:10:37 +0700

    [log-categorized-status-code] revision

--- log-categorized-status-code.py
+++ log-categorized-status-code.py
@@ -5,8 +5,10 @@ BASE_DIRECTORY="log-categorized-status-code"
 STATUS_CODES=[200, 201, 400, 401, 403, 404, 500, 502, 503]
 
 def create_directory():
+    os.makedirs(BASE_DIRECTORY, exist_ok=True)
     for status_code in STATUS_CODES:
-        os.makedirs(BASE_DIRECTORY+"/"+str(status_code), exist_ok=True)
+        log_file = open(BASE_DIRECTORY+"/"+str(status_code)+".log", "a")
+        log_file.close()
 
 def main():
     create_directory()
